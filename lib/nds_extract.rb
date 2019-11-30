2291e43601f76e952323f83b3fc6bce0cc886daf
Allen Zhang <azhang9328@gmail.com>
2019-11-30 05:20:06 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,6 +2,16 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-
- p nds[0][:movies][0][:worldwide_gross]
+  dirIndex = 0 
+  while dirIndex < nds.length do 
+    movieIndex = 0 
+    dirTotal = 0 
+    while movieIndex < nds[dirIndex][:movies].length do 
+    dirTotal = dirTotal + nds[dirIndex][:movies][movieIndex][:worldwid_gross]
+    movieIndex += 1   
+    end
+    result = {nds[dirIndex] => dirTotal}
+    dirIndex += 1 
+  end   
+  p result
 end
