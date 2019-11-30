4705c813a53926aee6a3bfb00a800f99b075a68b
Allen Zhang <azhang9328@gmail.com>
2019-11-30 05:17:01 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,16 +13,5 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  dirIndex = 0 
-  while dirIndex < nds.length do 
-    movieIndex = 0 
-    dirTotal = 0 
-    while movieIndex < nds[dirIndex][:movies].length do 
-    dirTotal += nds[dirIndex][:movies][movieIndex][:worldwid_gross]
-    movieIndex += 1   
-    end
-    result = {nds[dirIndex] => dirTotal}
-    dirIndex += 1 
-  end   
-  p result
+
 end
