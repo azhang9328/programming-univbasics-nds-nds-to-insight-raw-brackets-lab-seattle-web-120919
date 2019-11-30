64b639af8b7dfad9c6599b0bba31a0dd63f0532b
Allen Zhang <azhang9328@gmail.com>
2019-11-30 05:20:42 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,6 +4,7 @@ require 'directors_database'
 def directors_totals(nds)
   dirIndex = 0 
   while dirIndex < nds.length do 
+    puts "outerloop"
     movieIndex = 0 
     dirTotal = 0 
     while movieIndex < nds[dirIndex][:movies].length do 
