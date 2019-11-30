58232cc536bfe0e39abc3dd34b7f00d2c38e77f7
Allen Zhang <azhang9328@gmail.com>
2019-11-30 05:28:33 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,15 +5,14 @@ def directors_totals(nds)
   result = {}
   dirIndex = 0 
   while dirIndex < nds.length do 
-    puts "outerloop"
     movieIndex = 0 
     dirTotal = 0 
     while movieIndex < nds[dirIndex][:movies].length do 
-    puts "innerloop"
     dirTotal += nds[dirIndex][:movies][movieIndex][:worldwide_gross]
     puts dirTotal
     movieIndex += 1   
     end
+    puts nds[dirIndex][:name] + dirTotal
     result[":#{nds[dirIndex][:name]}"] = "#{dirTotal}" 
     dirIndex += 1 
   end   
